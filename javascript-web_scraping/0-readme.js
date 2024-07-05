d12860982d8692aaad61cc709520d7f283d8b775
pablosalina25 <casuarioventas@gmail.com>
2024-07-04 19:30:26 -0700

    0-readme.js

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,4 +1,18 @@
-#!/usr/bin/node
 // Write a script that reads and prints the content of a file.
 
+#!/usr/bin/node
+
 const fs = require('fs');
+
+const filePath = process.argv[2];
+
+function readFile(path) {
+  fs.readFile(path, 'utf-8', (error, data) => {
+    if (error) {
+      console.error(error);
+    } else {
+      console.log(data);
+    }
+  });
+}
+readFile(filePath);
