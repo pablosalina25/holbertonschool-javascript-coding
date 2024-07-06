2bb40548f37bcbfb88741e6bc5f63b4d35a6103d
pablosalina25 <casuarioventas@gmail.com>
2024-07-06 15:00:30 -0700

    5-request_store.js

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -1,27 +1,18 @@
 #!/usr/bin/node
-
-// Script that gets the contents of a webpage and stores it in a file.
-
+// A script that gets the contents of a webpage and stores it in a file.
 const request = require('request');
 const fs = require('fs');
-
 const url = process.argv[2];
 const filePath = process.argv[3];
 
-function getContent(url, filepath) {
-  request(url, { encoding: 'utf8' }, (error, response, body) => {
-    if (error) {
-      console.error('Error fetching webpage:', error);
-      return;
-    }
-    fs.writeFile(filepath, body, 'utf-8', (err) => {
+request(url, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  } else {
+    fs.writeFile(filePath, body, 'utf-8', (err) => {
       if (err) {
-        console.error('Error writing to file:', err);
-        return;
+        console.error(err);
       }
-      console.log(`Webpage content successfully saved to ${filepath}`);
     });
-  });
-}
-
-getContent(url, filePath);
+  }
+});
