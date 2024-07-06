0499e0fd9211b2b902457824df574e61dc80de6f
pablosalina25 <casuarioventas@gmail.com>
2024-07-06 14:32:57 -0700

    4-starwars_count.js

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -1,14 +1,17 @@
 #!/usr/bin/node
-// a script that prints the title of a Star Wars movie
-// where the episode number matches a given integer.
+
+// This script prints the title of a Star Wars movie where the episode number matches a given integer.
+
 const request = require('request');
 const movieId = process.argv[2];
-const url = https://swapi-api.hbtn.io/api/films/${movieId};
+const url = `https://swapi-api.hbtn.io/api/films/${movieId}`;
+
 request(url, function (error, response, body) {
-	if (error) {
-		console.error(error);
-	} else {
-		const movie = JSON.parse(body);
-		console.log(movie.title);
-	}
+  if (error) {
+    console.error(error);
+    return;
+  }
+  
+  const movie = JSON.parse(body);
+  console.log(movie.title);
 });
--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,15 +1,19 @@
 #!/usr/bin/node
-// A script that prints the number of movies where the character “Wedge Antilles” is present.
+
+// This script prints the number of movies where the character “Wedge Antilles” is present.
+
 const request = require('request');
-const api_Url = process.argv[2];
+const apiUrl = process.argv[2];
 const characterId = '18';
 
-request(api_Url, function (error, response, body) {
-	if (error) {
-		console.error(error);
-	} else {
-		const films = JSON.parse(body);
-		const count = films.results.filter((film) => film.characters.includes(https://swapi-api.hbtn.io/api/people/${characterId}/)).length;
-			console.log(count);
-		}
+request(apiUrl, function (error, response, body) {
+  if (error) {
+    console.error(error);
+    return;
+  }
+
+  const films = JSON.parse(body);
+  const count = films.results.filter((film) => film.characters.includes(`https://swapi-api.hbtn.io/api/people/${characterId}/`)).length;
+  
+  console.log(count);
 });
