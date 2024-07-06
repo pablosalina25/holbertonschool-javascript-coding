be0a13c8d0b629847fe105be07418ca0b2d09c9d
pablosalina25 <casuarioventas@gmail.com>
2024-07-06 14:46:09 -0700

    5-request_store.js

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -6,19 +6,22 @@ const request = require('request');
 const fs = require('fs');
 
 const url = process.argv[2];
-const filepath = process.argv[3];
+const filePath = process.argv[3];
 
-request(url, function (error, response, body) {
-  if (error) {
-    console.error('Error fetching webpage:', error);
-    return;
-  }
-
-  fs.writeFile(filepath, body, 'utf-8', function (err) {
-    if (err) {
-      console.error('Error writing to file:', err);
+function getContent(url, filepath) {
+  request(url, { encoding: 'utf8' }, (error, response, body) => {
+    if (error) {
+      console.error('Error fetching webpage:', error);
       return;
     }
-    console.log(`Webpage content successfully saved to ${filepath}`);
+    fs.writeFile(filepath, body, 'utf-8', (err) => {
+      if (err) {
+        console.error('Error writing to file:', err);
+        return;
+      }
+      console.log(`Webpage content successfully saved to ${filepath}`);
+    });
   });
-});
+}
+
+getContent(url, filePath);
