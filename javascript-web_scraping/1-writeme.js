2bec1d56161015a6b01cbe832b85dc51a8359b57
pablosalina25 <casuarioventas@gmail.com>
2024-07-09 08:48:52 -0700

    semi standart fix and make changes

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,13 +1,16 @@
 #!/usr/bin/node
-// A program writes a string to a file.
-
+// This script writes a string to a file.
 const fs = require('fs');
 
+if (process.argv.length !== 4) {
+  console.error('Usage: ./1-writeme.js <file_path> <string_to_write>');
+  process.exit(1);
+}
 const filePath = process.argv[2];
-const content = process.argv[3];
+const str = process.argv[3];
 
-fs.writeFile(filePath, content, 'utf-8', (err) => {
-	  if (err) {
-		      console.error(err);
-		    }
+fs.writeFile(filePath, str, 'utf8', (err) => {
+  if (err) {
+    console.log(err);
+  }
 });
--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -1,17 +1,19 @@
 #!/usr/bin/node
 
-// This script prints the title of a Star Wars movie where the episode number matches a given integer.
-
 const request = require('request');
-const movieId = process.argv[2];
-const url = `https://swapi-api.hbtn.io/api/films/${movieId}`;
 
-request(url, function (error, response, body) {
-  if (error) {
-    console.error(error);
-    return;
-  }
-  
-  const movie = JSON.parse(body);
-  console.log(movie.title);
-});
+const movieId = process.argv[2];
+
+const url = 'https://swapi-api.hbtn.io/api/films/' + movieId + '/';
+
+function getMovieTitle (url) {
+  request.get(url, (error, response, body) => {
+    if (error) {
+      console.error(error);
+      return;
+    }
+    const movieData = JSON.parse(body);
+    console.log(movieData.title);
+  });
+}
+getMovieTitle(url);
